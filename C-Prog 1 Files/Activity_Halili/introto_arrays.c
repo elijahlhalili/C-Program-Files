d6eeff7559bacac08ae4510a6cd4179b31564f72
elijahlhalili <elijahlhalili@gmail.com>
2025-11-17 13:25:26 +0800

    arraysssss

--- C-Prog 1 Files/Activity_Halili/introto_arrays.c
+++ C-Prog 1 Files/Activity_Halili/introto_arrays.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main ()
 {
+    system("cls");
 
+    /*
+        - Array is used to store many variables that are of same type only
+        - Aata structure is a format of organizing and storing data
+            and each data structure is designed to organize data to suit a specific purpose
+        - One dimensional array = the simplest form of array
+    */
 
+    // data_type (e.g. int) name_of_array (e.g arr) and [number elements]
+    int arr[5];
+    // array_name (e.g arr) [INDEX (the positon of your array) = e.g. 2]
+    arr[2];
 
+        //  EXAMPLE:
+    int number[5] = {5, 10, 15, 20, 25};
+    printf("%d\n", number[0]);
+        //reminder: that the first number always start with 0, so the 5 there is 0 for index
 
-    
-
+    int a[10], i;
+    for (i = 0; i < 10; i++){
+        printf("Enter the nuput for index %d: ", i);
+        scanf("%d", &a[i]);
+    }
+    printf("\nArray elements are as follows:\n");
+    for (i = 0; i < 10; i++){
+        printf("%d\t", a[i]);
+    }
 
     return 0;
 }
